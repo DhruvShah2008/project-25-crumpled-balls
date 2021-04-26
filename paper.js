be861651ea4ef3738dfc6f295010c1593b9f8104
DhruvShah2008 <78470667+DhruvShah2008@users.noreply.github.com>
2021-04-26 15:31:14 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -3,33 +3,36 @@ class paper
 	constructor(x,y,r)
 	{
 		var options={
-            isStatic:true,
-            restitution:0.3,
-            friction:0,
-            density:1.2			
+			isStatic:false,
+			restitution:0.3,
+			friction:0,
+			density:1.2
+
+			
 			}
 		this.x=x;
 		this.y=y;
-        this.r=r
-        this.image = loadImage("paper.png")
-		this.body=Bodies.rectangle(x, y, (this.r-20)/2 , options);
- 		World.add(world, this.body);
+		this.r=r
+		this.image=loadImage("paper.png");
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
 
 	}
 	display()
 	{
 			
-			var paperPos=this.body.position;		
+			var paperpos=this.body.position;		
 
 			push()
-			translate(paperPos.x, paperPos.y);
+			translate(paperpos.x, paperpos.y);
 			rectMode(CENTER)
 			//strokeWeight(4);
-			fill(128,128,128)
-			image(this.image,0,0,this.r,this.r);
+			fill(255,0,255)
+			imageMode(CENTER);
+			image(this.image, 0,0,this.r, this.r)
+			//ellipse(0,0,this.r, this.r);
 			pop()
 			
 	}
 
 }
-
